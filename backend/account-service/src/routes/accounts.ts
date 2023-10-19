01e9af9440d102e5e06fa02fa5c6dbabf92ccf0c
Wesley Borges <djwesleyborges@gmail.com>
2023-10-19 08:53:21 -0300

    separado middle

--- backend/account-service/src/routes/accounts.ts
+++ backend/account-service/src/routes/accounts.ts
@@ -1,27 +1,8 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 import accountsController from "../controllers/accounts";
-import { accountSchema, loginSchema } from "../models/account";
-import Joi from "joi";
+import { validateAccount, validateLogin } from "./middlewares";
 
 
-function validateSchema(schema: Joi.ObjectSchema<any>, req: Request, res: Response, next: any){
-    const { error } = schema.validate(req.body);
-    if (error == null) return next();
-
-    const { details } = error;
-    const message = details.map(item => item.message).join(',');
-
-    console.log(message);
-    res.status(422).end();
-}
-
-function validateAccount(req: Request, res: Response, next: any) {
-    return validateSchema(accountSchema, req, res, next)
-}
-
-function validateLogin(req: Request, res: Response, next: any) {
-    return validateSchema(loginSchema, req, res, next)
-}
 
 const router = Router();
 
